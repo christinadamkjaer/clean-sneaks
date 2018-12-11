8bab84cce55fd43308a3f1c0d58004bfb450f3cb
Matias Skødt <matiasskodt@gmail.com>
2018-12-11 11:31:57 +0100

    twenty twenty slider

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+/* https://www.w3schools.com/w3css/w3css_slideshow.asp */
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -8,10 +10,20 @@ function plusDivs(n) {
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("anbefaling");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
+    if (n > x.length) {
+        slideIndex = 1
     }
-    x[slideIndex-1].style.display = "block"; 
+    if (n < 1) {
+        slideIndex = x.length
+    };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
 }
+
+$(function () {
+    $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({
+        default_offset_pct: 0.7
+    });
+});
